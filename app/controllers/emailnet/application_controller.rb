c2dadb7f71e462eefed04602bd3b93d648a8aa44
Jake Webber <jake@webbernet.com.au>
2020-05-08 15:37:24 +1000

    Added auth

--- app/controllers/emailnet/application_controller.rb
+++ app/controllers/emailnet/application_controller.rb
@@ -1,5 +1,13 @@
 module Emailnet
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :http_authenticate
+
+    def http_authenticate
+      return true unless Rails.env == 'production'
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'username' && password == ENV['DEV_PASSWORD']
+      end
+    end
   end
 end
